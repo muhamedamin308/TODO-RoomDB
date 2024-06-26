99f7db2c1c3a17a3ecdae8397e096bc970dbbdb9
muhamedamin308 <mohamed.amin.2290@gmail.com>
2024-06-26 23:16:41 +0300

    user experience

--- app/src/main/java/com/example/roomtodo/ui/tasks/AllTasksFragment.kt
+++ app/src/main/java/com/example/roomtodo/ui/tasks/AllTasksFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -37,6 +38,8 @@ class AllTasksFragment : Fragment() {
         binding.filterTasks.setOnClickListener {
             liveFilter = !liveFilter
             if (liveFilter) {
+                Toast.makeText(requireContext(), "Shows only completed tasks", Toast.LENGTH_SHORT)
+                    .show()
                 viewModel.allCompletedTasks.observe(viewLifecycleOwner) {
                     tasksAdapter.setTask(it)
                 }
